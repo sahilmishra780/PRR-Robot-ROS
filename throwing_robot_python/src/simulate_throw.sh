256dd0d063abc625e49c30e73cf091e0143cb91d
Sahil Mishra <sahilmishra780@gmail.com>
2020-05-03 00:38:37 -0500

    bash script to run the complete simulation

--- throwing_robot_python/src/simulate_throw.sh
+++ throwing_robot_python/src/simulate_throw.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+rostopic pub -1 /throwing_robot/joint1_position_controller/command std_msgs/Float64 "data: -0.45"
 rosrun throwing_robot_python initialize_robot.sh
-rosrun throwing_robot_python trajectory.py
+rosrun throwing_robot_python trajectory_large.py
+rosrun throwing_robot_python reset_to_zero.sh
+rostopic pub -1 /throwing_robot/joint1_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /throwing_robot/joint1_position_controller/command std_msgs/Float64 "data: 0.45"
+rosrun throwing_robot_python initialize_robot.sh
+rosrun throwing_robot_python trajectory_small.py
 rosrun throwing_robot_python reset_to_zero.sh
